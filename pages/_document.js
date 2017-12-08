32ccda33265ef56192465f5b9981e9dbec233629
Paitoon <rimb9pkmink@gmail.com>
2017-12-08 19:55:01 +0700

    remove meta tag viewport

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,6 @@ export default class MyDocument extends Document {
       <html>
         <Head>
             <meta charSet="UTF-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <meta httpEquiv="X-UA-Compatible" content="ie=edge"/>
             <title>Insurance App</title>
             <link href="https://fonts.googleapis.com/css?family=Prompt" rel="stylesheet" />
